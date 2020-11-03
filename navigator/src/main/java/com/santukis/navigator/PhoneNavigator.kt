8198ebf39b5c5dc5b9a6e630d2877197df6c2c86
David <santamadavid@gmail.com>
2020-11-03 19:38:27 +0100

    loadFragment revisited to support onConfiguration changes

--- navigator/src/main/java/com/santukis/navigator/PhoneNavigator.kt
+++ navigator/src/main/java/com/santukis/navigator/PhoneNavigator.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 
 internal class PhoneNavigator(activity: AppCompatActivity) : Navigator(activity) {
 
-    override fun getContainerFor(fragmentFactory: FragmentFactory): Int = fragmentFactory.getPortraitContainer()
+    override fun getContainerFor(fragmentFactory: FragmentFactory): Int? = fragmentFactory.getPortraitContainer()
 
     override fun isLastFragment(): Boolean {
         return activity.get()?.supportFragmentManager?.backStackEntryCount ?: -1 <= 1
