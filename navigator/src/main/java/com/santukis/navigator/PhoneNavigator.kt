9bf803524df6d5c3674ae4afba7c9fdd741ab051
David <santamadavid@gmail.com>
2020-12-30 16:34:42 +0100

    revisited isLastFragment

--- navigator/src/main/java/com/santukis/navigator/PhoneNavigator.kt
+++ navigator/src/main/java/com/santukis/navigator/PhoneNavigator.kt
@@ -7,6 +7,6 @@ internal class PhoneNavigator(activity: AppCompatActivity) : Navigator(activity)
     override fun getContainerFor(fragmentFactory: FragmentFactory): Int? = fragmentFactory.getPortraitContainer()
 
     override fun isLastFragment(): Boolean {
-        return activity.get()?.supportFragmentManager?.backStackEntryCount ?: -1 <= 1
+        return activity.get()?.let { it.supportFragmentManager.backStackEntryCount <= 1 } ?: false
     }
 }
